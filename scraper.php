84c955e4f0d6bfabe5679981a46d651d027bdfe6
vikash12345 <vikash@backgroundcheck.email>
2017-02-08 15:57:28 +0500

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -27,7 +27,7 @@
 
 
 
-<?
+
 // This is a template for a PHP scraper on morph.io (https://morph.io)
 // including some code snippets below that you should find helpful
 // require 'scraperwiki.php';
